4c4d782ded53aa45cff8654155a65bd085d4d5ea
Liam G Khidyer <liamgkhidyer@gmail.com>
2026-02-07 21:08:18 +0000

    fixed nect issue

---
+++ components/buttons/NavBtns.tsx
@@ -0,0 +1,17 @@
+
+
+export function LoginBtn() {
+    return (
+        <button className="w-full md:w-auto bg-blue-500 text-white px-4 py-2 rounded-md">
+            Login
+        </button>
+    );
+}
+
+export function SignupBtn() {
+    return (
+        <button className="w-full md:w-auto bg-green-500 text-white px-4 py-2 rounded-md">
+            Signup
+        </button>
+    );
+}
